0ab445279730f6140ff4c066de54f57cecc93fb4
LYthe <2270779418@qq.com>
2018-06-13 18:48:03 +0800

    hsgd

---
+++ Regression/regression_1.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x, y = [], []
+for sample in open("E:\PycharmProjects\machine_learning\_Data\prices.txt", "r"):
+    _x, _y = sample.split(",")
+    x.append(float(_x))
+    y.append(float(_y))
+x, y = np.array(x), np.array(y)
+x = (x-x.mean())/x.std()
+plt.figure()
+plt.scatter(x, y, c='g', s=6)
+plt.show()
